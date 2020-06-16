1ca745ed2441b7ee5c8c35899228e4f82ce321e1
spencerdouglas7 <dougla55@purdue.edu>
2020-06-16 16:15:29 -0400

    start of fullcalendar working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  $("#demo").html("Hello, World!");
+  $("#demo").html("jQuery working");
+  $("#panel1").slideUp(300).slideDown(1000);
+  $("#panel2").click(function() {
+  	$("#panel2").hide();
+
+  }); 
 });
