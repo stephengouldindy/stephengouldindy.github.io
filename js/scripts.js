d61ac532d5dd29651bb25c2041a1ee6bad28d2d3
spencerdouglas7 <dougla55@purdue.edu>
2020-06-18 13:39:13 -0400

    finished date-specific event adding

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ $("#incomingbtn").click(function() {
 		if (origValue === "btn btn-outline-primary") {
 			//flip other button
 			$("#outgoingbtn").attr("class", "btn btn-outline-primary");
+			//toggle button states for good practice
 			return "btn btn-primary";
 		}
 		else return "btn btn-primary";
@@ -22,6 +23,7 @@ $("#incomingbtn").click(function() {
 /*
  * Flip swap outgoing and incoming buttons if the outgoing is pressed and is not enabled
  */
+
 $("#outgoingbtn").click(function() {
 	$("#outgoingbtn").attr("class", function(i, origValue){
 		if (origValue === "btn btn-outline-primary") {
@@ -32,3 +34,63 @@ $("#outgoingbtn").click(function() {
 		else return "btn btn-primary";
 	});
 }); 
+
+/*
+ * Clear delivery window time boxes
+ */
+
+
+
+ //FIXME: RESET BUTTON DOES NOT WORK
+
+
+
+
+ $("#clearTimeBtn").click(function() {
+ 	console.log($("#startTime").val());
+ 	console.log($("#endTime").val());
+ 	$("#startTime").attr("value", "--:--");
+ 	$("#endTime").attr("value", "--:--");
+ });
+
+/*
+ * Schedule truck: collect form data, wrap into event obj, and place it in the calendar and hide the form.
+ 
+  $("#submitTruckBtn").click(function() {
+  	//unwrapping data...
+ 	let startTime = $("#startTime").val();
+ 	let endTime = $("#endTime").val();
+ 	let date = $("#dateAlert").html();
+ 	let customerName = $("#customerBox").val();
+	let vendorName = $("#vendorBox").val();
+ 	let destination = $("#destinationBox").val();
+ 	console.log(startTime);
+ 	console.log(endTime);
+ 	console.log(date);
+ 	console.log(customerName);
+ 	console.log(vendorName);
+ 	console.log(destination);
+
+ 	//packing data into object
+	var newEvent = {
+              title: vendorName,
+              start: startTime,
+              end: endTime,
+              allDay: false,
+              editable: true
+
+    };
+ 	//insert new object into calendar as event 
+	calendar.addEvent({
+	              title: "TestEvent",
+	              start: new Date(2020,5,18, 13,30,0),
+	              end: new Date(2020,5,18,16,30,0),
+	              allDay: false
+	    	})
+
+
+ 	//hide form
+
+ 	//scroll to top
+ });
+ */
